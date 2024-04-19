c5d615db8b74cada2e16a8e7e5da2a8bfdbc339f
vincenzo <vincenzo.paparella@outlook.it>
2024-04-19 16:50:15 +0200

    updt best epoch

--- best_epoch.py
+++ best_epoch.py
@@ -5,7 +5,83 @@ import os
 
 datasets = ['facebook_books', 'amazon_baby', 'ml-1m']
 for dataset in datasets:
-    for file in glob.glob(f'results/{dataset}/performance/*'):
+    for file in glob.glob(f'results/{dataset}/performance/*None*'):
+        with open(file, 'rb') as handle:
+            store_validation = pickle.load(handle)
+
+        for k, v in store_validation.items():
+            store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+        maximumValue = max(store_validation.values(), key=lambda k: k[1])
+        maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+
+        print(maxKey, maximumValue)
+        rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        if not os.path.exists(f'results/{dataset}/best_recs'):
+            os.makedirs(f'results/{dataset}/best_recs')
+        dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        shutil.copyfile(rec, dst)
+        loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+        dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+        shutil.copyfile(loss, dst_loss)
+
+    for file in glob.glob(f'results/{dataset}/performance/*multifr*'):
+        with open(file, 'rb') as handle:
+            store_validation = pickle.load(handle)
+
+        for k, v in store_validation.items():
+            store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+        maximumValue = max(store_validation.values(), key=lambda k: k[1])
+        maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+
+        print(maxKey, maximumValue)
+        rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        if not os.path.exists(f'results/{dataset}/best_recs'):
+            os.makedirs(f'results/{dataset}/best_recs')
+        dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        shutil.copyfile(rec, dst)
+        loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+        dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+        shutil.copyfile(loss, dst_loss)
+
+    for file in glob.glob(f'results/{dataset}/performance/*PDU*75*'):
+        with open(file, 'rb') as handle:
+            store_validation = pickle.load(handle)
+
+        for k, v in store_validation.items():
+            store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+        maximumValue = max(store_validation.values(), key=lambda k: k[1])
+        maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+
+        print(maxKey, maximumValue)
+        rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        if not os.path.exists(f'results/{dataset}/best_recs'):
+            os.makedirs(f'results/{dataset}/best_recs')
+        dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        shutil.copyfile(rec, dst)
+        loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+        dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+        shutil.copyfile(loss, dst_loss)
+
+    for file in glob.glob(f'results/{dataset}/performance/*PDU*25*'):
+        with open(file, 'rb') as handle:
+            store_validation = pickle.load(handle)
+
+        for k, v in store_validation.items():
+            store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+        maximumValue = max(store_validation.values(), key=lambda k: k[1])
+        maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+
+        print(maxKey, maximumValue)
+        rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        if not os.path.exists(f'results/{dataset}/best_recs'):
+            os.makedirs(f'results/{dataset}/best_recs')
+        dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+        shutil.copyfile(rec, dst)
+        loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+        dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+        shutil.copyfile(loss, dst_loss)
+
+    for file in glob.glob(f'results/{dataset}/performance/*PDU*0$5*'):
         with open(file, 'rb') as handle:
             store_validation = pickle.load(handle)
 
