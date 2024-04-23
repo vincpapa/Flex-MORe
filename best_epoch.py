f4614840166b7d1fce867200f7733d32bae8abbf
vincenzo <vincenzo.paparella@outlook.it>
2024-04-23 15:53:08 +0200

    best epoch updt

--- best_epoch.py
+++ best_epoch.py
@@ -50,7 +50,7 @@ for dataset in datasets:
             store_validation = pickle.load(handle)
 
         for k, v in list(store_validation.items()):
-            if '0$75' not in k:
+            if '0[$]75' not in k:
                 del store_validation[k]
         for k, v in store_validation.items():
             store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
@@ -73,7 +73,7 @@ for dataset in datasets:
             store_validation = pickle.load(handle)
 
         for k, v in list(store_validation.items()):
-            if '0$25' not in k:
+            if '0[$]25' not in k:
                 del store_validation[k]
         for k, v in store_validation.items():
             store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
@@ -96,7 +96,7 @@ for dataset in datasets:
             store_validation = pickle.load(handle)
 
         for k, v in list(store_validation.items()):
-            if '0$5' not in k:
+            if '0[$]5' not in k:
                 del store_validation[k]
         for k, v in store_validation.items():
             store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
@@ -119,7 +119,7 @@ for dataset in datasets:
             store_validation = pickle.load(handle)
 
         for k, v in list(store_validation.items()):
-            if '0$9' not in k:
+            if '0[$]9' not in k:
                 del store_validation[k]
         for k, v in store_validation.items():
             store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
