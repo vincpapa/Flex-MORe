e5c60b99876c24a1bfb87290826234724e8ecc1e
vincenzo <vincenzo.paparella@outlook.it>
2024-04-25 17:12:32 +0200

    best epoch

--- best_epoch.py
+++ best_epoch.py
@@ -6,7 +6,7 @@ import os
 
 datasets = ['amazon_music']
 backbone = 'BPRMF'
-method = 'multifr'
+method = 'PDU'
 for dataset in datasets:
     if method == 'None':
         for file in glob.glob(f'results/{dataset}/performance/*{backbone}*None*'):
