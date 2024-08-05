7abb987ce0c791814efc8fb8b28b5304821d29ff
vincenzo <vincenzo.paparella@outlook.it>
2024-08-05 19:02:04 +0200

    conf file

--- show_dict.py
+++ show_dict.py
@@ -3,7 +3,7 @@ from glob import glob
 
 # Path to the pickle file
 data = 'facebook_books'
-pickle_file_path = f'results/{data}/performance/'
+pickle_file_path = f'results/{data}/performance/*BPRMF*'
 for file in glob(f'{pickle_file_path}*'):
     with open(file, 'rb') as file_1:
         loaded_dict = pickle.load(file_1)
