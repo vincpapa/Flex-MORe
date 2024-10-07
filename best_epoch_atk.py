32c36c310c5e7056c5c58ac78deb830ca15a3c4c
vincenzo <vincenzo.paparella@outlook.it>
2024-10-07 10:08:09 +0200

    best epoch atk 20 30

--- best_epoch_atk.py
+++ best_epoch_atk.py
@@ -130,6 +130,30 @@ for dataset in datasets:
                     dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
                     shutil.copyfile(loss, dst_loss)
 
+        if method == 'FLEXMORE':
+
+            for file in glob.glob(f'results/{dataset}/performance/*{backbone}*rpms*'):
+                with open(file, 'rb') as handle:
+                    store_validation = pickle.load(handle)
+                for k, v in list(store_validation.items()):
+                    if 'atk_con=20-atk_pro=30' not in k:
+                        del store_validation[k]
+                for k, v in store_validation.items():
+                    store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+                if store_validation != {}:
+                    maximumValue = max(store_validation.values(), key=lambda k: k[1])
+                    maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+
+                    print(maxKey, maximumValue)
+                    rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+                    if not os.path.exists(f'results/{dataset}/best_recs'):
+                        os.makedirs(f'results/{dataset}/best_recs')
+                    dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+                    shutil.copyfile(rec, dst)
+                    loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+                    dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+                    shutil.copyfile(loss, dst_loss)
+
 
 
 
