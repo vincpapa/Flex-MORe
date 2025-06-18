9da805f44da38277b6b3d976405271e4a23a18b4
vincenzo <vincenzo.paparella@outlook.it>
2025-06-18 12:12:57 +0200

    best epoch flexmore lightgcn

--- best_epoch_scale.py
+++ best_epoch_scale.py
@@ -4,7 +4,7 @@ import shutil
 import os
 
 
-datasets = ['facebook_books'] # ['amazon_music'] # ['amazon_baby', 'facebook_books']
+datasets = ['amazon_baby'] # ['amazon_music'] # ['amazon_baby', 'facebook_books']
 backbones = ['LightGCN'] # ['BPRMF','NGCF']
 method = 'FLEXMORE_SCALE'
 scales = ['0$25', '0$5', '0$75', '0$95'] # ['0$25', '0$3', '0$4', '0$5', '0$6', '0$7', '0$75']
