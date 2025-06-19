b0a13ca27a9023a64298274ebce2effb7ad23cdc
vincenzo <vincenzo.paparella@outlook.it>
2025-06-19 14:57:43 +0200

    best epoch

--- best_epoch_prefe.py
+++ best_epoch_prefe.py
@@ -4,8 +4,8 @@ import shutil
 import os
 
 
-datasets = ['amazon_baby'] # ['amazon_music'] # ['amazon_baby', 'facebook_books']
-backbones = ['LightGCN'] # ['BPRMF','NGCF']
+datasets = ['facebook_books'] # ['amazon_music'] # ['amazon_baby', 'facebook_books']
+backbones = ['BPRMF'] # ['BPRMF','NGCF','LightGCN']
 method = 'PREFEADAFLEXMORE'
 scales = ['0$25', '0$5'] # ['0$25', '0$3', '0$4', '0$5', '0$6', '0$7', '0$75']
 for dataset in datasets:
