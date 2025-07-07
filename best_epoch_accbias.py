3c2e702bb5945b97f736cf6ffc690e6be1ddb578
vincenzo <vincenzo.paparella@outlook.it>
2025-07-07 10:52:19 +0200

    accuracy bias best epoch

--- best_epoch_accbias.py
+++ best_epoch_accbias.py
@@ -14,25 +14,28 @@ for dataset in datasets:
             if method == '=ADAFLEXMORE':
 
                 for file in glob.glob(f'results/{dataset}/performance/*{backbone}*rpm*'):
-                    with open(file, 'rb') as handle:
-                        store_validation = pickle.load(handle)
-                    for k, v in list(store_validation.items()):
-                        # if scale not in k:
-                        if f'acc_bias={bias}' in k:
-                            del store_validation[k]
-                    for k, v in store_validation.items():
-                        store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
-                    if store_validation != {}:
-                        maximumValue = max(store_validation.values(), key=lambda k: k[1])
-                        maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
-                        if method in maxKey:
-                            print(maxKey, maximumValue)
-                            rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
-                            if not os.path.exists(f'results/{dataset}/best_recs'):
-                                os.makedirs(f'results/{dataset}/best_recs')
-                            dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
-                            shutil.copyfile(rec, dst)
-                            loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
-                            dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
-                            shutil.copyfile(loss, dst_loss)
+                    try:
+                        with open(file, 'rb') as handle:
+                            store_validation = pickle.load(handle)
+                        for k, v in list(store_validation.items()):
+                            # if scale not in k:
+                            if f'acc_bias={bias}' in k:
+                                del store_validation[k]
+                        for k, v in store_validation.items():
+                            store_validation[k] = sorted(v, key=lambda x: x[1], reverse=True)[0]
+                        if store_validation != {}:
+                            maximumValue = max(store_validation.values(), key=lambda k: k[1])
+                            maxKey = next(k for k, v in store_validation.items() if v == maximumValue)
+                            if method in maxKey:
+                                print(maxKey, maximumValue)
+                                rec = f'results/{dataset}/recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+                                if not os.path.exists(f'results/{dataset}/best_recs'):
+                                    os.makedirs(f'results/{dataset}/best_recs')
+                                dst = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_recs.tsv'
+                                shutil.copyfile(rec, dst)
+                                loss = f'results/{dataset}/losses/{maxKey}_loss.pkl'
+                                dst_loss = f'results/{dataset}/best_recs/{maxKey}_it={maximumValue[0]}_loss.pkl'
+                                shutil.copyfile(loss, dst_loss)
+                    except:
+                        pass
 
