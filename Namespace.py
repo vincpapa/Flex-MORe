c7853b619387cb249cb3bf9f8a22626221bf009b
vincenzo <vincenzo.paparella@outlook.it>
2025-07-29 15:19:25 +0200

    CALFLEXMORE

--- Namespace.py
+++ Namespace.py
@@ -60,7 +60,7 @@ class Namespace:
             self.ranker = experiment['ranker']
         elif self.mo_method == 'None':
             self.scale1 = experiment['scale']
-        elif self.mo_method in ['FLEXMORE_SCALE', 'FLEXMORE_ABL', 'FLEXMORE_ABL_WOS', 'FLEXMORE_ABL_WOZ', 'FLEXMORE_EPO', 'CALFLEXMORE']:
+        elif self.mo_method in ['FLEXMORE_SCALE', 'FLEXMORE_ABL', 'FLEXMORE_ABL_WOS', 'FLEXMORE_ABL_WOZ', 'FLEXMORE_EPO']:
             try:
                 self.atk_con = experiment['atk']['atk_cons']
             except KeyError:
